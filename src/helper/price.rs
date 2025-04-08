c826cba3741fa7f4003a95819ef75cf40b6d0584
Phudit Chuengjaroen <phudit.nut@gmail.com>
2025-04-09 00:40:48 +0700

    feat: round up price

--- src/helper/price.rs
+++ src/helper/price.rs
@@ -1,6 +1,7 @@
 // TODO: need check round up
 pub fn round_up_float_to_one_precision(num: f64) -> String {
-    let fmt_value = format!("{:.1}", num);
+    let value = (num * 10.0).ceil() / 10.0;
+    let fmt_value = format!("{:.1}", value);
     if fmt_value == String::from("-0.0") {
         return String::from("0.0");
     }
@@ -8,7 +9,8 @@ pub fn round_up_float_to_one_precision(num: f64) -> String {
 }
 
 pub fn round_up_float_to_two_precision(num: f64) -> String {
-    let fmt_value = format!("{:.2}", num);
+    let value = (num * 100.0).ceil() / 100.0;
+    let fmt_value = format!("{:.2}", value);
     if fmt_value == String::from("-0.00") {
         return String::from("0.00");
     }
